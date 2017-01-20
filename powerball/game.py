c1a3e5b8441795203c12287256f4308365f7a28b
Vivek Pabani <vpabani@hawk.iit.edu>
2017-01-19 23:59:52 -0600

    update add more player prompt - allow any key for no

--- powerball/game.py
+++ powerball/game.py
@@ -44,7 +44,7 @@ class Game:
 
             # True if user enters 'Y' or 'y'. False for any other input.
 
-            add_more = (input("Do you want to add another employee? (y/n) : ").strip().lower() == 'y')
+            add_more = (input("\nDo you want to add another player? (y for yes/any key for no) : ").strip().lower() == 'y')
 
     def generate_winning_numbers(self):
         """
