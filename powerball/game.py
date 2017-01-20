3fc605bca3cc22b191a9ec9ad9d8b2bb2fa60c65
Vivek Pabani <vpabani@hawk.iit.edu>
2017-01-19 23:29:34 -0600

    add method to generate the winning number

--- powerball/game.py
+++ powerball/game.py
@@ -46,6 +46,42 @@ class Game:
 
             add_more = (input("Do you want to add another employee? (y/n) : ").strip().lower() == 'y')
 
+    def generate_winning_numbers(self):
+        """
+        Generate the winning number based on the players numbers.
+        The 5 white balls are generated from the top 5 white ball numbers entered by all players.
+        The 6th power ball is generated from the maximum occuring user power balls.
+        If any of those choices have ties, they are resolved randomly.
+        Add the winning numbers to the winning_numbers.
+        """
+
+        # Players added by the begin or add_player method are by default verified.
+        # In case the players are initialized at the time of game instance creation,
+        # we should check for player validity before generating the winning numbers.
+
+        for player in self.players:
+            if not isinstance(player, Player) or not player.is_valid_player():
+                print("Some or all players are invalid.")
+                return
+
+        # get the list of white balls and power balls entered by all players.
+        wb_nums = self.get_white_ball_numbers()
+        pb_nums = self.get_power_ball_numbers()
+
+        # Counter returns the counter object with count of each number in the list.
+        # Counter.most_common(i) retuns the maximum occuring top i numbers from the list.
+        # In case of ties between counts, most_common returns arbitary number,
+        # which fulfills our condition of resolving the ties by choosing a random number.
+        # so no extra check required for ties between counts.
+
+        top_five_wb = [item[0] for item in Counter(wb_nums).most_common(5)]
+        top_pb = Counter(pb_nums).most_common(1)[0][0]
+
+        # add the winning numbers to the list.
+
+        self.winning_numbers.extend(top_five_wb)
+        self.winning_numbers.append(top_pb)
+
     def get_white_ball_numbers(self):
         """
         Get the white ball numbers from all the players and return as a list.
