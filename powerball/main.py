3884f3510bd1b69c288d34670a863d14a4e91c87
Vivek Pabani <vpabani@hawk.iit.edu>
2017-01-20 01:03:53 -0600

    add simulation steps comments to main

--- powerball/main.py
+++ powerball/main.py
@@ -13,6 +13,15 @@ from .player import Player
 
 def main():
 
+    # The game simulation is done with following steps:
+    #
+    # 1. Create a game instance with no players.
+    # 2. Invoke the begin method, which asks for player data and 
+    #    favorite numbers until stopped.
+    # 3. Once players data is entered, invoke the method to generate 
+    #    winning number.
+    # 4. Display players data and winning numbers. 
+
     print('\n')
 
     game = Game()
