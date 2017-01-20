cc60f639850e1b969b961aae09e25250bf35a2d6
Vivek Pabani <vpabani@hawk.iit.edu>
2017-01-19 19:32:29 -0600

    add test for get_name.

--- tests/test_player.py
+++ tests/test_player.py
@@ -19,7 +19,6 @@ class TestPlayerClass:
         assert player.last_name == None
         assert player.numbers == []
 
-
     def test_init_with_all_arguments_not_none(self):
 
         first_name = "Foo"
@@ -31,3 +30,19 @@ class TestPlayerClass:
         assert player.first_name == first_name
         assert player.last_name == last_name
         assert player.numbers == numbers
+
+    @mock.patch('builtins.input', side_effect=['Foo'])
+    def test_get_name_with_valid_input_foo_non_blank(self, input):
+    
+        name = Player._get_name('name_type')
+        assert name == 'Foo'
+    
+    @mock.patch('builtins.input', side_effect=['', '   ', '\n', 'Foo'])
+    def test_get_name_with_invalid_inputs_blank_spaces_newline(self, input):
+        """
+        multiple inputs are provided, since initial invalid
+        inputs will result in next promt until valid value is entered.
+        """
+
+        name = Player._get_name('name_type')
+        assert name == 'Foo'
