f597073ac91bd44e78c30045f2c5526907d6f1a6
Vivek Pabani <vpabani@hawk.iit.edu>
2017-01-19 21:32:12 -0600

    test begin game with 3 players

--- tests/test_game.py
+++ tests/test_game.py
@@ -38,3 +38,24 @@ class TestGameClass:
         assert player.first_name == input_names[0]
         assert player.last_name == input_names[1]
         assert player.numbers == input_numbers
+
+    def test_begin_with_adding_3_players(self):
+
+        game = Game()
+
+        assert len(game.players) == 0
+
+        input_names = ['Foo', 'Bar']
+        input_numbers = [2, 5, 8, 13, 45, 24]
+
+        input_data = input_names + input_numbers
+
+        res_y = ['y']
+        res_n = ['n']
+
+        three_player_data = input_data + res_y + input_data + res_y + input_data + res_n
+
+        with mock.patch('builtins.input', side_effect=three_player_data):
+            game.begin()
+
+        assert len(game.players) == 3
