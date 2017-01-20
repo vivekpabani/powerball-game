9a76a1d63f29c40c65b6b68b4cb55bbdba2024cd
Vivek Pabani <vpabani@hawk.iit.edu>
2017-01-19 21:26:35 -0600

    add test for add_player in game

--- tests/test_game.py
+++ tests/test_game.py
@@ -17,3 +17,24 @@ class TestGameClass:
 
         game = Game()
         assert isinstance(game, Game) == True
+
+    def test_add_player(self):
+
+        game = Game()
+
+        assert len(game.players) == 0
+
+        input_names = ['Foo', 'Bar']
+        input_numbers = [2, 5, 8, 13, 45, 24]
+
+        input_data = input_names + input_numbers
+
+        with mock.patch('builtins.input', side_effect=input_data):
+            game.add_player()
+
+        assert len(game.players) == 1
+        player = game.players[0]
+
+        assert player.first_name == input_names[0]
+        assert player.last_name == input_names[1]
+        assert player.numbers == input_numbers
